f56b67a29a1aa6507f52e7e4c968006cf3b0569d
yeezynico <yeezy77680@gmail.com>
2025-03-30 21:12:31 +0200

    add t at chaatt

--- src/components/projet/projet.jsx
+++ src/components/projet/projet.jsx
@@ -12,7 +12,7 @@ export default function Projet() {
 
             <div className="flex justify-center flex-wrap gap-26">
                 <Project_card title="Puisssance 4 :" text="création d'un site jeux de puissance 4." src="puissance4.png" alt="puissance4" href='https://github.com/yeezynico/Puissance-4' path="https://puissance-4-two.vercel.app/" path_name="Lien vercel"/>
-                <Project_card title="Chapate :" text="Site pour acheter des photos de chat." src="chapate.png" alt="site de photo de chat" href='https://github.com/yeezynico/chapatte' />
+                <Project_card title="Chapatte :" text="Site pour acheter des photos de chat." src="chapate.png" alt="site de photo de chat" href='https://github.com/yeezynico/chapatte' />
                 <Project_card title="Epitweet :" text="Reproduction du site Tweeter."src="epitweet.png" alt="site epitweet" href='https://github.com/yeezynico/Epitweet' />
                 <Project_card title="Menu :" text="Ce site permet de voir les cartes des restaurateurs pour savoir ce que le restaurant propose au menu." src="menu.png" alt="site d'un menu" href='https://github.com/yeezynico/MENU_V2' />
                 <Project_card text="projet en cour !" src="" alt="" />
